d1b36c417800c3fff5814952356900bdbe3ec927
Riley Martin <wenjanmartin@gmail.com>
2022-10-03 13:07:20 -0400

    Add script for setting up fresh installs

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+install() {
+  git clone https://github.com/andsens/homeshick.git ~/.homesick/repos/homeshick
+  source ~/.homesick/repos/homeshick/homeshick.sh
+}
+
+install_dotfiles() {
+  homeshick clone riley-martin/dotfiles
+}
+
+install
+install_dotfiles
